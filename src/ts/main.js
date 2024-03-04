5ed9ef48dfccc5208a9668432a04154f9f77762a
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-03-04 22:33:33 +0300

    Create layout, class ToDo, create new task, delete task, loading all task in html, save storage, write on storage

--- src/ts/main.js
+++ src/ts/main.js
@@ -7,7 +7,6 @@ var Category;
 var ToDo = /** @class */ (function () {
     // конструктор класса для загрузки localStorage
     function ToDo() {
-        // Загрузка данных из localStorage при создании экземпляра ToDo
         var _this = this;
         this._tasks = []; // list tasks
         // приватная фунция для загрузки localStorage
@@ -41,6 +40,12 @@ var ToDo = /** @class */ (function () {
             _this._tasks.filter(function (task) { return task.id === id; });
             _this.saveTasks(); // сохраняем tasks после удаления
         };
+        this.clear = function () {
+            _this._tasks = []; // чистка localstorage
+            _this.saveTasks();
+        };
+        // Загрузка данных из localStorage при создании экземпляра ToDo
+        this.loadTask();
     }
     Object.defineProperty(ToDo.prototype, "tasks", {
         // функция для получения всех задач @GET
@@ -54,7 +59,86 @@ var ToDo = /** @class */ (function () {
 }());
 // загрузка класс через event List
 // DOMContentLoaded === useEffect
+var todo = new ToDo(); // создание экмепляра
+localStorage.setItem('tasks', JSON.stringify(todo.tasks));
+// фунция для загрузки задач
+var loadingTasks = function () {
+    var ul = document.getElementById('list');
+    ul.innerHTML = '';
+    todo.tasks.forEach(function (task) {
+        var li = document.createElement('li');
+        li.classList.add('toDo__item'); // Добавления класса
+        // Создание div container card
+        var cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        // Создание левой части card
+        var leftSide = document.createElement('div');
+        leftSide.classList.add('card__left');
+        // Добавление радио кнопки
+        var radioInput = document.createElement('input');
+        radioInput.type = 'radio';
+        radioInput.classList.add('card__radio');
+        radioInput.checked = task.isFinish;
+        // Добавление текстового поля
+        var textInput = document.createElement('input');
+        textInput.type = 'text';
+        textInput.classList.add('card__input');
+        textInput.value = task.value;
+        // Добавление радио кнопки и текстового поля в левую часть карточки
+        leftSide.appendChild(radioInput);
+        leftSide.appendChild(textInput);
+        // Создание правой части card
+        var rightSide = document.createElement('div');
+        rightSide.classList.add('card__right');
+        // Добавление кнопок в правую часть карточки
+        var editButton = document.createElement('button');
+        editButton.classList.add('card__btn', 'purple');
+        editButton.textContent = 'Edit';
+        var deleteButton = document.createElement('button');
+        deleteButton.classList.add('card__btn', 'red');
+        deleteButton.textContent = 'Delete';
+        rightSide.appendChild(editButton);
+        rightSide.appendChild(deleteButton);
+        // Добавление левой и правой части карточки в общий контейнер карточки
+        cardDiv.appendChild(leftSide);
+        cardDiv.appendChild(rightSide);
+        li.appendChild(cardDiv);
+        ul.appendChild(li);
+        console.log('task');
+    });
+};
 document.addEventListener("DOMContentLoaded", function () {
-    var todo = new ToDo(); // создание экземпряла
     console.log(todo.tasks);
+    loadingTasks(); //загрузка все задач
 });
+// btn проверяем btn
+var onSubmitBtn = function (event) {
+    event.preventDefault(); // блокировка обновления сайта
+    var input = document.getElementById('toDo__input');
+    console.log(input);
+    loadingTasks(); // загрузка все задач
+};
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    var input = document.getElementById('toDo__input'); // type input
+    var radios = document.getElementsByName('category');
+    console.log(input.value);
+    radios.forEach(function (radio) {
+        if (radio.checked) {
+            console.log(radio.id);
+        }
+    });
+    todo.createTasks({
+        title: input.value,
+        value: input.value,
+        category: Category.Business,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        id: todo.tasks.length + 1,
+        isFinish: false
+    });
+    console.log(todo.tasks);
+    loadingTasks(); //загрузка все задач
+});
+todo.clear();
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -20,7 +20,7 @@ class ToDo {
     // конструктор класса для загрузки localStorage
     constructor() {
         // Загрузка данных из localStorage при создании экземпляра ToDo
-
+        this.loadTask();
     }
 
     // приватная фунция для загрузки localStorage
@@ -61,14 +61,119 @@ class ToDo {
         this._tasks.filter((task) => task.id === id)
         this.saveTasks(); // сохраняем tasks после удаления
     }
-}
 
+    clear = () => {
+        this._tasks = []; // чистка localstorage
+        this.saveTasks();
+    }
+}
 
 // загрузка класс через event List
 // DOMContentLoaded === useEffect
 
-document.addEventListener("DOMContentLoaded", () => {
-    const todo = new ToDo(); // создание экземпряла
+const todo = new ToDo(); // создание экмепляра
+localStorage.setItem('tasks', JSON.stringify(todo.tasks));
 
+// фунция для загрузки задач
+const loadingTasks = () => {
+    const ul = document.getElementById('list');
+    ul.innerHTML = '';
+    todo.tasks.forEach((task) => {
+        const li = document.createElement('li');
+        li.classList.add('toDo__item'); // Добавления класса
+
+        // Создание div container card
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+
+        // Создание левой части card
+        const leftSide = document.createElement('div');
+        leftSide.classList.add('card__left')
+
+        // Добавление радио кнопки
+        const radioInput = document.createElement('input');
+        radioInput.type = 'radio';
+        radioInput.classList.add('card__radio');
+        radioInput.checked = task.isFinish;
+
+        // Добавление текстового поля
+        const textInput = document.createElement('input');
+        textInput.type = 'text';
+        textInput.classList.add('card__input');
+        textInput.value = task.value;
+
+        // Добавление радио кнопки и текстового поля в левую часть карточки
+        leftSide.appendChild(radioInput);
+        leftSide.appendChild(textInput);
+
+
+        // Создание правой части card
+        const rightSide = document.createElement('div');
+        rightSide.classList.add('card__right')
+
+        // Добавление кнопок в правую часть карточки
+        const editButton = document.createElement('button');
+        editButton.classList.add('card__btn', 'purple');
+        editButton.textContent = 'Edit';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('card__btn', 'red');
+        deleteButton.textContent = 'Delete';
+
+        rightSide.appendChild(editButton);
+        rightSide.appendChild(deleteButton);
+
+        // Добавление левой и правой части карточки в общий контейнер карточки
+        cardDiv.appendChild(leftSide);
+        cardDiv.appendChild(rightSide);
+
+        li.appendChild(cardDiv);
+        ul.appendChild(li);
+        console.log('task')
+    })
+}
+document.addEventListener("DOMContentLoaded", () => {
     console.log(todo.tasks);
+    loadingTasks(); //загрузка все задач
 })
+
+
+// btn проверяем btn
+const onSubmitBtn = (event) => {
+    event.preventDefault(); // блокировка обновления сайта
+
+    const input = document.getElementById('toDo__input');
+    console.log(input);
+    loadingTasks(); // загрузка все задач
+
+}
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const input = document.getElementById('toDo__input') as HTMLInputElement; // type input
+    const radios = document.getElementsByName('category') as NodeListOf<HTMLInputElement>
+
+    console.log(input.value);
+    radios.forEach((radio) => {
+        if (radio.checked) {
+            console.log(radio.id)
+        }
+    })
+
+    todo.createTasks({
+        title: input.value,
+        value: input.value,
+        category: Category.Business,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        id: todo.tasks.length + 1,
+        isFinish: false
+    })
+    console.log(todo.tasks)
+    loadingTasks(); //загрузка все задач
+})
+
+todo.clear();
